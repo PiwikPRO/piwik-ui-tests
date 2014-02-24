054fa25aab8514fa176b6c5e81ab13fdc975956b
diosmosis <benakamoorthi@fastmail.fm>
2014-02-24 23:49:20 +0000

    Remove debugging code. [ci skip]

--- travis.sh
+++ travis.sh
@@ -3,8 +3,6 @@ echo "View UI failures (if any) here http://builds-artifacts.piwik.org/ui-tests.
 echo "If the new screenshots are valid, then you can copy them over to tests/PHPUnit/UI/expected-ui-screenshots/."
 echo ""
 
-echo `pwd`
-
 if [ -f ./UI/run-tests.js ]; then
     phantomjs ./UI/run-tests.js
 else
