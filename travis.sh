917c4431036a75cefd357ca1511e1891cd7f8265
diosmosis <benakamoorthi@fastmail.fm>
2014-02-24 05:25:32 +0000

    Make travis run JS screenshot tests.

---
+++ travis.sh
@@ -0,0 +1,6 @@
+echo ""
+echo "View UI failures (if any) here http://builds-artifacts.piwik.org/ui-tests.master/$TRAVIS_JOB_NUMBER/screenshot-diffs/diffviewer.html"
+echo "If the new screenshots are valid, then you can copy them over to tests/PHPUnit/UI/expected-ui-screenshots/."
+echo ""
+
+phantomjs ./UI/run-tests.js
