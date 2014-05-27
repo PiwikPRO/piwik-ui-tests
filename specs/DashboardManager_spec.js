15e1892a561bf7c669d0303e13185860b857a29f
Thomas Steur <thomas.steur@googlemail.com>
2014-05-28 00:14:45 +0200

    we no longer need the widgetized dashboard manager since we can run the screenshot tests within an existing page

--- specs/DashboardManager_spec.js
+++ specs/DashboardManager_spec.js
@@ -8,38 +8,39 @@
  */
 
 describe("DashboardManager", function () {
+
     this.timeout(0);
 
-    var url = "?module=Widgetize&action=iframe&moduleToWidgetize=Dashboard&actionToWidgetize=getDashboardSettingsControl&"
-            + "&idSite=1&period=day&date=2012-01-01&includeWidgetFactory=1";
+    var selectorToCapture = '.dashboard-manager';
+    var url = "?module=CoreHome&action=index&idSite=1&period=day&date=2012-01-01&includeWidgetFactory=1";
 
     it("should load correctly", function (done) {
-        expect.screenshot("loaded").to.be.capture(function (page) {
+        expect.screenshot("loaded").to.be.captureSelector(selectorToCapture, function (page) {
             page.load(url);
         }, done);
     });
 
     it("should expand when clicked", function (done) {
-        expect.screenshot("expanded").to.be.capture(function (page) {
+        expect.screenshot("expanded").to.be.captureSelector(selectorToCapture, function (page) {
             page.click('.dashboard-manager');
         }, done);
     });
 
     it("should show widget for a category when category label hovered", function (done) {
-        expect.screenshot("widget_list_shown").to.be.capture(function (page) {
+        expect.screenshot("widget_list_shown").to.be.captureSelector(selectorToCapture, function (page) {
             page.mouseMove('.widgetpreview-categorylist>li:contains(Live!)'); // have to mouse move twice... otherwise Live! will just be highlighted
             page.mouseMove('.widgetpreview-categorylist>li:contains(Visits Summary)');
         }, done);
     });
 
     it("should load a widget preview when a widget is hovered", function (done) {
-        expect.screenshot("widget_preview").to.be.capture(function (page) {
+        expect.screenshot("widget_preview").to.be.captureSelector(selectorToCapture, function (page) {
             page.mouseMove('.widgetpreview-widgetlist>li:contains(Visits Over Time)');
         }, done);
     });
 
     it("should close the manager when a widget is selected", function (done) {
-        expect.screenshot("loaded").to.be.capture("widget_selected", function (page) {
+        expect.screenshot("loaded").to.be.captureSelector("widget_selected", selectorToCapture, function (page) {
             // make sure selecting a widget does nothing
             page.evaluate(function () {
                 $('.dashboard-manager').data('uiControlObject').widgetSelected = function () {};
