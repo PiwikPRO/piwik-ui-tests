127b668342799b8b82044754cd485ace43437068
Thomas Steur <thomas.steur@googlemail.com>
2014-05-27 23:59:31 +0200

    we no longer need the widgetized site selector since we can run the screenshot tests within an existing page

--- specs/SiteSelector_spec.js
+++ specs/SiteSelector_spec.js
@@ -8,31 +8,32 @@
  */
 
 describe("SiteSelector", function () {
+    var selectorToCapture = '[piwik-siteselector],[piwik-siteselector] .custom_select';
+
     this.timeout(0);
 
-    var url = "?module=Widgetize&action=iframe&moduleToWidgetize=CoreHome&actionToWidgetize=getSiteSelector&"
-            + "&idSite=1&period=year&date=2012-08-09";
+    var url = "?module=UsersManager&action=userSettings&idSite=1&period=day&date=yesterday";
 
     it("should load correctly", function (done) {
-        expect.screenshot("loaded").to.be.capture(function (page) {
+        expect.screenshot("loaded").to.be.captureSelector(selectorToCapture, function (page) {
             page.load(url);
         }, done);
     });
 
     it("should display expanded when clicked", function (done) {
-        expect.screenshot("expanded").to.be.capture(function (page) {
+        expect.screenshot("expanded").to.be.captureSelector(selectorToCapture, function (page) {
             page.click('.sites_autocomplete');
         }, done);
     });
 
     it("should show no results when search returns no results", function (done) {
-        expect.screenshot("search_no_results").to.be.capture(function (page) {
+        expect.screenshot("search_no_results").to.be.captureSelector(selectorToCapture, function (page) {
             page.sendKeys(".websiteSearch", "abc");
         }, done);
     });
 
     it("should search when one character typed into search input", function (done) {
-        expect.screenshot("search_one_char").to.be.capture(function (page) {
+        expect.screenshot("search_one_char").to.be.captureSelector(selectorToCapture, function (page) {
             page.click('.reset');
             page.sendKeys(".websiteSearch", "s");
         }, done);
@@ -40,14 +41,14 @@ describe("SiteSelector", function () {
 
     // Test is skipped as it randomly fails http://builds-artifacts.piwik.org/ui-tests.master/2295.1/screenshot-diffs/diffviewer.html
     it.skip("should search again when second character typed into search input", function (done) {
-        expect.screenshot("search_two_chars").to.be.capture(function (page) {
+        expect.screenshot("search_two_chars").to.be.captureSelector(selectorToCapture, function (page) {
             page.sendKeys(".websiteSearch", "st");
             page.wait(3000);
         }, done);
     });
 
     it("should change the site when a site is selected", function (done) {
-        expect.screenshot("site_selected").to.be.capture(function (page) {
+        expect.screenshot("site_selected").to.be.captureSelector(selectorToCapture, function (page) {
             page.click(".custom_select_ul_list>li:visible");
         }, done);
     });
