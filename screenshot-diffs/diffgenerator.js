63ea53008dd59373bdbd4eda26dd87d70f665151
Thomas Steur <thomas.steur@googlemail.com>
2014-05-28 04:29:52 +0200

    this allows us to generate a diff client side which does not fail when screenshots have different sizes

---
+++ screenshot-diffs/diffgenerator.js
@@ -0,0 +1,49 @@
+resemble.outputSettings({
+    errorColor: {
+        red: 255,
+        green: 0,
+        blue: 0,
+        alpha: 125
+    },
+    errorType: 'movement',
+    transparency: 0.3
+});
+
+function compareImages(expected, expectedGithub, processed)
+{
+    resemble(processed).compareTo(expected).onComplete(function(data){
+
+        var info = 'Mismatch percentage: ' + data.misMatchPercentage + '%';
+
+        if (data.dimensionDifference && !data.isSameDimensions) {
+            info += ' Dimension difference width: ' + data.dimensionDifference.width + ' height: ' + data.dimensionDifference.height;
+        }
+
+        $('.info').text(info);
+        $('.diff').attr('src', data.getImageDataUrl());
+    });
+
+    $('.expected')[0].src = expected;
+    $('.expectedGithub')[0].src = expectedGithub;
+    $('.processed')[0].src = processed;
+}
+
+function getUrlQueryParam(sParam) {
+    var query     = window.location.search.substring(1);
+    var variables = query.split('&');
+
+    for (var index = 0; index < variables.length; index++) {
+
+        var sParameterName = variables[index].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}
+
+$(function () {
+    var processed = getUrlQueryParam('processed');
+    var expected  = getUrlQueryParam('expected');
+    var github    = getUrlQueryParam('github');
+    compareImages(expected, github, processed);
+});
