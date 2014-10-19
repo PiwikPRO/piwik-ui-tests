15cbdb081a9d75cd9e83c013215d321acca6ca41
Thomas Steur <thomas.steur@googlemail.com>
2014-10-19 23:41:30 +0200

    refs #4996 added some more UI tests. They will most likely fail as the fixture is not updated yet

--- specs/UIIntegration_spec.js
+++ specs/UIIntegration_spec.js
@@ -180,7 +180,25 @@ describe("UIIntegrationTest", function () { // TODO: Rename to Piwik?
 
     it('should load the actions > contents page correctly', function (done) {
         expect.screenshot('actions_contents').to.be.captureSelector('.pageWrap,.expandDataTableFooterDrawer', function (page) {
-            page.load("?" + urlBase + "#" + generalParams + "&module=Contents&action=index");
+            page.load("?" + urlBase + "#" + generalParams + "&module=Contents&action=index&period=day&date=2012-01-01");
+        }, done);
+    });
+
+    it("should show all corresponding content pieces when clicking on a content name", function (done) {
+        expect.screenshot("loaded").to.be.captureSelector("action_content_name_piece", '.pageWrap', function (page) {
+            page.click('.dataTable .subDataTable .value:contains(ImageAd)');
+        }, done);
+    });
+
+    it("should show all tracked content pieces when clicking on the table", function (done) {
+        expect.screenshot("loaded").to.be.captureSelector("action_content_piece", '.pageWrap', function (page) {
+            page.click('.reportDimension .dimension:contains(Content Piece)');
+        }, done);
+    });
+
+    it("should show all corresponding content names when clicking on a content piece", function (done) {
+        expect.screenshot("loaded").to.be.captureSelector("action_content_piece_name", '.pageWrap', function (page) {
+            page.click('.dataTable .subDataTable .value:contains(Click NOW)');
         }, done);
     });
 
