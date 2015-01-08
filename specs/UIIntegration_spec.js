685845790470e6c0e10d9bd853130545e23c809a
Thomas Steur <thomas.steur@gmail.com>
2015-01-08 02:02:39 +0000

    finally figured out why the goals manage screen did not work. The URL was wrong and the selector as well. fixed!

--- specs/UIIntegration_spec.js
+++ specs/UIIntegration_spec.js
@@ -248,9 +248,9 @@ describe("UIIntegrationTest", function () { // TODO: Rename to Piwik?
     });
 
     it('should load the goals > management page correctly', function (done) {
-        expect.screenshot('goals_manage').to.be.captureSelector('.pageWrap,.expandDataTableFooterDrawer', function (page) {
-            page.load( "?" + urlBase + "#" + generalParams + "&module=Goals&action=manage");
-            page.wait(500);
+        expect.screenshot('goals_manage').to.be.captureSelector('.centerLargeDiv,.top_bar_sites_selector,.entityContainer', function (page) {
+            page.load( "?" + generalParams + "&module=Goals&action=manage");
+            page.wait(200);
         }, done);
     });
 
